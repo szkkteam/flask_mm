d97576f6440efaa9c2abe6442ae3f386ba1518f0
Istvan Rusics <szkkteam1@gmail.com>
2020-02-04 10:11:57 +0100

    Missing requirements updated

--- flask_mm/__about__.py
+++ flask_mm/__about__.py
@@ -7,5 +7,5 @@ from __future__ import unicode_literals
 # Pip package imports
 # Internal package imports
 
-__version__ = '0.0.8.dev'
+__version__ = '0.0.9.dev'
 __description__ = 'Media Manager for flask'
